bf38b5ef70b98cb44d9861e246310e0b1be8e1cc
Elias <knoll.elias@icloud.com>
2023-05-09 13:22:24 +0200

    made it work

--- App.tsx
+++ App.tsx
@@ -13,10 +13,10 @@ import { OverlayProvider, Chat, Theme, DeepPartial } from "stream-chat-expo";
 import AuthContext from "./src/contexts/AuthContext";
 import { StreamColors } from "./src/constants/Colors";
 import { Amplify, Auth } from "aws-amplify";
-import { withAuthenticator } from "aws-amplify-react-native";
-import awsconfig from "./src/aws-exports";
+// import { withAuthenticator } from "aws-amplify-react-native";
+// import awsconfig from "./src/aws-exports";
 
-Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
+// Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 const API_KEY = "tmdq3ta7ah6k";
 const client = StreamChat.getInstance(API_KEY);
